abfac12b91402c012072f9a2358c1662283683bf
Shib Chandan Mistry <shibchandan11@gmail.com>
2025-04-20 22:29:18 +0530

    Added frontend code

---
+++ frontend/static/script.js
@@ -0,0 +1,23 @@
+function getPrediction() {
+    const text = document.getElementById("newsInput").value;
+  
+    fetch("http://127.0.0.1:5000/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ text })
+    })
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("result").innerText = data.prediction;
+      document.getElementById("card").style.transform = "rotateY(180deg)";
+    })
+    .catch(err => {
+      console.error(err);
+      alert("Something went wrong.");
+    });
+  }
+  
+
+
